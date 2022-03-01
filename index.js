19a72cb5f3fdefa84001adc654830698583d9573
HiccupAashih <shahaashish22@gmail.com>
2022-03-01 14:22:03 +1100

    Test passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution here!
+// Write your solution here!p
+const cats=["Milo","Otis","Garfield"];
+const destructivelyAppendCat=(name)=> {return cats.push(name);}
+const destructivelyPrependCat=(name)=>{return cats.unshift(name);}
+const destructivelyRemoveLastCat=(name)=>{return cats.pop(name)}
+const destructivelyRemoveFirstCat=(name)=>{return cats.shift(name);}
+const appendCat=()=> {return[...cats,'Broom']};
+const prependCat=()=> {return['Arnold',...cats]};
+const removeLastCat=()=>{return cats.slice(0,-1,);}
+const removeFirstCat=()=>{ return cats.slice(1);}
